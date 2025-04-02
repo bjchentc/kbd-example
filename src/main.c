a8ef1f101cb27c822f57d75f939760d10a7d2e63
BJ Chen <bj.chen@ite.com.tw>
2025-04-02 10:35:43 +0800

    New Keyboard Example with Event-driven Thread
    1. Replacing deprecated KSCAN
    2. Successfully tested with ITE EVB.
    
    Signed-off-by: BJ Chen <bj.chen@ite.com.tw>

--- src/main.c
+++ src/main.c
@@ -2,26 +2,26 @@
 #include <stdio.h>
 #include <soc.h>
 #include <zephyr/kernel.h>
+#include <zephyr/input/input.h>
+#include <zephyr/input/input_kbd_matrix.h>
+#include "main.h"
 
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
-#ifdef CONFIG_KSCAN
-#include <zephyr/drivers/kscan.h>
-
 #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD
 K_EVENT_DEFINE(ec_event);
-#define USER_STACKSIZE_KSCAN    1024
-K_THREAD_STACK_DEFINE(kbmatrix_stack, USER_STACKSIZE_KSCAN);
+#define USER_STACKSIZE_KBD    1024
+K_THREAD_STACK_DEFINE(kbd_stack, USER_STACKSIZE_KBD);
 
-struct k_thread kbmatrix_thread;
-void keyboard_matrix_thread(void *p1, void *p2, void *p3)
+struct k_thread kbd_thread;
+void kbd_service_thread(void *p1, void *p2, void *p3)
 {
     while (1) {
         k_event_wait(&ec_event, BIT(0), false, K_FOREVER);
 
         #ifdef CONFIG_ITE_EC_KBD_DBG
-        LOG_INF("Key Scan Service Triggered by kbd callback event.");
+        LOG_INF("Keyboard Service Triggered by kbd input callback event.");
         #endif /* #ifdef CONFIG_ITE_EC_KBD_DBG */
 
         k_event_clear(&ec_event, BIT(0));
@@ -32,44 +32,67 @@ void keyboard_matrix_thread(void *p1, void *p2, void *p3)
 }
 #endif /* #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD */
 
-const struct device *const kscan_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_keyboard_scan));
 
-static void kb_callback(const struct device *dev, uint32_t row, uint32_t col,
-			bool pressed)
+#define EC_KEYBOARD_NODE DT_CHOSEN(ec_keyboard)
+static const struct device *const kbd_dev = DEVICE_DT_GET(EC_KEYBOARD_NODE);
+
+static void keyboard_input_cb(struct input_event *evt, void *user_data)
 {
-	ARG_UNUSED(dev);
-    #ifdef CONFIG_ITE_EC_KBD_DBG
-	LOG_INF("Key [%u,%u] Pressed = %u\n", row,col, pressed);
-    #endif /* #ifdef CONFIG_ITE_EC_KBD_DBG */
+    static int row;
+    static int col;
+    static bool pressed;
 
-    #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD
-    k_event_post(&ec_event, BIT(0));
-    #endif /* #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD */
-}
+    switch (evt->code) {
+    case INPUT_ABS_X:
+        col = evt->value;
+        break;
+    case INPUT_ABS_Y:
+        row = evt->value;
+        break;
+    case INPUT_BTN_TOUCH:
+        pressed = evt->value;
+        break;
+    }
 
-int kbd_init(const struct device *dev)
-{
-	if (!device_is_ready(kscan_dev)) {
+    if (evt->sync) {
         #ifdef CONFIG_ITE_EC_KBD_DBG
-		LOG_ERR("kscan device %s not ready", kscan_dev->name);
+        LOG_INF("Keyboard Event Triggered: [x, y, pressed] = [%d, %d, %d]\r\n", col, row, pressed);
         #endif /* #ifdef CONFIG_ITE_EC_KBD_DBG */
-		return -1;
-	}
 
-	kscan_config(kscan_dev, kb_callback);
+        #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD
+        k_event_post(&ec_event, BIT(0));
+        #endif /* #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD */
+    }
+}
+INPUT_CALLBACK_DEFINE(kbd_dev, keyboard_input_cb,NULL);
+
+int kbd_init(void)
+{
+    if (!device_is_ready(kbd_dev)) {
+
+        #ifdef CONFIG_ITE_EC_KBD_DBG
+		LOG_ERR("kbd device %s not ready", kbd_dev->name);
+        #endif /* #ifdef CONFIG_ITE_EC_KBD_DBG */
+
+        return -1;
+    }
+
+    input_kbd_matrix_pm_action(kbd_dev, 1);
+
+    LOG_INF("Keyboard Initialization Complete\r\n");
 
     return 0;
 }
-#endif /* CONFIG_KSCAN */
+
 
 int main(void)
 {
     #ifdef CONFIG_ITE_KBD_HOOK_EVENT_THREAD
 	k_thread_create(
-        &kbmatrix_thread,
-        kbmatrix_stack,
-        K_THREAD_STACK_SIZEOF(kbmatrix_stack),
-	    keyboard_matrix_thread,
+        &kbd_thread,
+        kbd_stack,
+        K_THREAD_STACK_SIZEOF(kbd_stack),
+	    kbd_service_thread,
         NULL,
         NULL,
         NULL,
@@ -79,10 +102,8 @@ int main(void)
     );
     #endif /* CONFIG_ITE_KBD_HOOK_EVENT_THREAD */
 
-#ifdef CONFIG_KSCAN
     LOG_INF("ITE Keyboard Example Enabled.\r\n");
-    kbd_init(kscan_dev);
-#endif /* #ifdef CONFIG_KSCAN */
+    kbd_init();
 
 	return 0;
 }
---
+++ src/main.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2025 ITE Tech. Inc.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#define MAKE_EVENT              0
+#define BREAK_EVENT             1
+#define REPEAT_EVENT            2
+
+int kbd_init(void);
+
+#endif /* #ifndef MAIN_H */
